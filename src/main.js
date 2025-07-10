96882f340f6656fb5c4400e477b997380dfda838
CrazyRyan0812 <ryan2409072@gmail.com>
2025-07-10 15:58:28 +0800

    250710, After the course

--- src/main.js
+++ src/main.js
@@ -1,10 +1,28 @@
 import { createApp } from 'vue'
 import './style.css'
 import { createPinia } from 'pinia'
+import { createMemoryHistory, createRouter } from "vue-router"
 import App from './App.vue'
 
-const pinia = createPinia()
-const app = createApp(App)
+// const pinia = createPinia()
+// const app = createApp(App)
 
-app.use(pinia)
-app.mount('#app')
+// app.use(pinia)
+// app.mount('#app')
+
+import RegisterForm from "./components/users/RegisterForm.vue"
+import LoginForm from "./components/users/LoginForm.vue"
+
+const routes = [
+  { path: "/register", component: RegisterForm },
+  { path: "/login", component: LoginForm },
+]
+
+const router = createRouter({
+  history: createMemoryHistory(),
+  routes,
+})
+
+const pinia = createPinia()
+
+createApp(App).use(pinia).use(router).mount("#app")
